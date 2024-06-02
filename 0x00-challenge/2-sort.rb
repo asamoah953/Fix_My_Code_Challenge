70268df5ed3c36a9e42861f960f09823fc00189f
asamoah953 <asamoahclinton953@gmail.com>
2024-06-02 04:13:13 -0700

    2-sort.rb

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -1,4 +1,8 @@
-
+###
+#
+#  Sort integer arguments (ascending) 
+#
+###
 
 result = []
 ARGV.each do |arg|
@@ -16,7 +20,7 @@ ARGV.each do |arg|
         if result[i] < i_arg
             i += 1
         else
-            result.insert(i - 1, i_arg)
+            result.insert(i, i_arg)
             is_inserted = true
             break
         end
